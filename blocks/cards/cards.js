5091a0e13855b679348befa4a5ce2c632c431da0
Bryan Stopp <bstopp@users.noreply.github.com>
2023-04-27 10:57:34 -0500

    Use icons in Card View. (#60)
    * Use icons in Card View.
    
    * Fix ilnt.
    
    * Hide stuff on mobile if needed.

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,16 +1,11 @@
 export default function decorate(block) {
   // add logic for check-list case
-  if (block.classList.contains('cards-checklist')) {
+  if (block.classList.contains('icons')) {
     block.classList.add('cards-2-cols');
     [...block.children].forEach((row) => {
       row.className = 'cards-item';
-      [...row.children].forEach((div) => {
-        div.className = 'cards-card-body';
-      });
-      const checkbox = document.createElement('div');
-      checkbox.className = 'checkmark';
-      const description = row.querySelector('.cards-card-body');
-      row.append(checkbox, description);
+      row.children[0].classList.add('card-icon');
+      row.children[1].classList.add('card-body');
     });
   } else {
     const cols = block.children;
@@ -20,14 +15,14 @@ export default function decorate(block) {
       [...row.children].forEach((div) => {
         if (div.querySelector('picture')) {
           // update container for picture with label
-          div.className = 'cards-card-image';
+          div.classList.add('card-image');
           if (div.lastChild.nodeType === Node.TEXT_NODE) {
             const picture = div.querySelector('picture');
             const paragraphElement = document.createElement(('p'));
             paragraphElement.append(div.lastChild);
             div.append(picture, paragraphElement);
           }
-        } else div.className = 'cards-card-body';
+        } else div.classList.add('card-body');
       });
     });
   }
