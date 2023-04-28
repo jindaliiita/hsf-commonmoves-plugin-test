c565b24e093e54eb398ccb756a0e3d3804537ba7
Bryan Stopp <bstopp@users.noreply.github.com>
2023-04-28 15:41:32 -0500

    Add support for title to Cards block. (#67)
    * Add support for title to Cards block.
    
    * Lint

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,16 +1,32 @@
 export default function decorate(block) {
+  const cards = [...block.children];
+
+  // Check for a title row
+  if (block.children[0].children.length === 1) {
+    const wrapper = cards.shift();
+    const title = wrapper.children[0];
+    title.classList.add('title');
+    wrapper.remove();
+    block.prepend(title);
+  }
+
+  const list = document.createElement('div');
+  list.classList.add('cards-list');
+  block.append(list);
+
   // add logic for check-list case
   if (block.classList.contains('icons')) {
     block.classList.add('cards-2-cols');
-    [...block.children].forEach((row) => {
+    cards.forEach((row) => {
       row.className = 'cards-item';
       row.children[0].classList.add('card-icon');
       row.children[1].classList.add('card-body');
+      list.append(row);
     });
   } else {
     const cols = block.children;
     block.classList.add(`cards-${cols.length}-cols`);
-    [...block.children].forEach((row) => {
+    cards.forEach((row) => {
       row.className = 'cards-item';
       [...row.children].forEach((div) => {
         if (div.querySelector('picture')) {
@@ -24,6 +40,7 @@ export default function decorate(block) {
           }
         } else div.classList.add('card-body');
       });
+      list.append(row);
     });
   }
 }
