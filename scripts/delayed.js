75d527318dce25d1db8a3ebb6b3719cb365c65e8
Rob Rusher <rrusher@adobe.com>
2024-02-06 11:33:54 -0700

    remove onetrust code

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,11 +1,8 @@
 // eslint-disable-next-line import/no-cycle
-import { sampleRUM, loadScript, fetchPlaceholders } from './aem.js';
+import { sampleRUM, loadScript } from './aem.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
-
-// fetch placeholders file
-const placeholders = await fetchPlaceholders();
 
 /*
   * Returns the environment type based on the hostname.
@@ -31,31 +28,5 @@ async function loadAdobeLaunch() {
     async: true,
   });
 }
-
-// OneTrust Cookies Consent Notice start
-/* if (!window.location.host.includes('hlx.page') && !window.location.host.includes('localhost')) {
-  const otId = placeholders.onetrustid;
-  if (otId) {
-    loadScript('https://cdn.cookielaw.org/scripttemplates/otSDKStub.js', {
-      type: 'text/javascript',
-      charset: 'UTF-8',
-      'data-domain-script': `${otId}`,
-    });
-
-    window.OptanonWrapper = () => {
-    };
-  }
-
-  const allButtons = document.querySelectorAll('a.button');
-  allButtons.forEach((button) => {
-    if (button.getAttribute('href').includes('cookie-policy')) {
-      button.addEventListener('click', (e) => {
-        // eslint-disable-next-line no-undef
-        OneTrust.ToggleInfoDisplay();
-        e.preventDefault();
-      });
-    }
-  });
-} */
 
 if (!window.location.host.includes('localhost')) await loadAdobeLaunch();
