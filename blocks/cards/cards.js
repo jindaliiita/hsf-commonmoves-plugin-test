f0629a381d94a58fe0e64050c44b4626a2720735
Rob Rusher <rrusher@adobe.com>
2024-04-30 19:05:16 -0600

    moved offices to block

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -51,7 +51,85 @@ function buildCards(block, cards) {
   }
 }
 
-async function fetchOffices(url) {
+function buildOfficeCards(list, data) {
+  data.forEach((item) => {
+    const cardsItem = document.createElement('div');
+    cardsItem.className = 'cards-item';
+    const cardImage = document.createElement('div');
+    cardImage.className = 'card-image';
+    const image = createOptimizedPicture(item.image, item.location, true);
+    image.style = 'padding-bottom: 75%';
+    const type = document.createElement('p');
+    type.innerText = item.type;
+    cardImage.append(image, type);
+    cardsItem.append(cardImage);
+    const cardBody = document.createElement('div');
+    cardBody.className = 'card-body';
+    const location = document.createElement('h3');
+    location.innerText = item.location;
+    const address = document.createElement('p');
+    address.innerHTML = item.address;
+    address.innerHTML += `<br/>${item.cityStateZip}`;
+    address.innerHTML += `<br/>Office: ${phoneFormat(item.phone)}`;
+    if (item.fax) address.innerHTML += `<br/>Office Fax: ${phoneFormat(item.fax)}`;
+    const contact = document.createElement('p');
+    contact.innerHTML = `<b>${item.contactName}</b>`;
+    contact.innerHTML += `<br/>${item.contactTitle}`;
+    contact.innerHTML += `<br/>${item.contactPhone}`;
+    contact.innerHTML += `<br/>${item.contactEmail}`;
+    const contactBtn = document.createElement('p');
+    contactBtn.className = 'button-container';
+    contactBtn.innerHTML = `<a href="/offices/${toClassName(item.location)}" title="Visit Us" class="button">Visit Us</a>`;
+    cardBody.append(location, address, contact, contactBtn);
+    cardsItem.append(cardBody);
+    list.append(cardsItem);
+  });
+}
+
+function buildAgentCards(list, data) {
+  const { pathname } = window.location;
+  const parts = pathname.split('/');
+  const pageName = parts[parts.length - 1];
+  const filteredData = data.filter((item) => item.office.toLowerCase() === pageName);
+  filteredData.forEach((item) => {
+    const cardsItem = document.createElement('div');
+    cardsItem.className = 'cards-item';
+    const cardImage = document.createElement('div');
+    cardImage.className = 'card-image';
+    const tmpImage = 'https://main--hsf-commonmoves--hlxsites.hlx.page/media/images/no-profile-image.png';
+    const image = createOptimizedPicture(tmpImage, item.name, true);
+    image.style = 'padding-bottom: 75%';
+
+    cardImage.append(image);
+    cardsItem.append(cardImage);
+    const cardBody = document.createElement('div');
+    cardBody.className = 'card-body';
+    const nameLink = document.createElement('a');
+    nameLink.className = 'name';
+    nameLink.href = item.profile;
+    nameLink.innerText = item.name;
+    const title = document.createElement('div');
+    title.className = 'title';
+    title.innerText = item.title;
+    const team = document.createElement('div');
+    team.className = 'teamname';
+    team.innerText = item.team;
+    const phone = document.createElement('div');
+    phone.className = 'phone';
+    phone.innerText = phoneFormat(item.phone);
+    const license = document.createElement('div');
+    license.className = 'license';
+    license.innerText = item.license;
+    const contactBtn = document.createElement('p');
+    contactBtn.className = 'button-container';
+    contactBtn.innerHTML = `<a href="${item.profile}" title="Agent Detail" class="button">Agent Detail</a>`;
+    cardBody.append(nameLink, title, team, phone, license, contactBtn);
+    cardsItem.append(cardBody);
+    list.append(cardsItem);
+  });
+}
+
+async function fetchIndex(url) {
   const response = await fetch(url);
   const json = await response.json();
   return json.data;
@@ -70,45 +148,18 @@ export default async function decorate(block) {
   }
   // Check for JSON data
   if (cards.length === 1 && cards[0].children.length === 1) {
-    const url = cards.shift();
-    const offices = await fetchOffices(url.querySelector('a[href]').href);
-    url.remove();
+    const link = cards.shift();
+    const url = new URL(link.querySelector('a[href]').href);
+    const jsonData = await fetchIndex(url.href);
+    link.remove();
     const list = document.createElement('div');
     list.classList.add('cards-list');
     block.append(list);
-    offices.forEach((office) => {
-      const cardsItem = document.createElement('div');
-      cardsItem.className = 'cards-item';
-      const cardImage = document.createElement('div');
-      cardImage.className = 'card-image';
-      const image = createOptimizedPicture(office.image, office.location, true);
-      image.style = 'padding-bottom: 75%';
-      const type = document.createElement('p');
-      type.innerText = office.type;
-      cardImage.append(image, type);
-      cardsItem.append(cardImage);
-
-      const cardBody = document.createElement('div');
-      cardBody.className = 'card-body';
-      const location = document.createElement('h3');
-      location.innerText = office.location;
-      const address = document.createElement('p');
-      address.innerHTML = office.address;
-      address.innerHTML += `<br/>${office.cityStateZip}`;
-      address.innerHTML += `<br/>Office: ${phoneFormat(office.phone)}`;
-      if (office.fax) address.innerHTML += `<br/>Office Fax: ${phoneFormat(office.fax)}`;
-      const contact = document.createElement('p');
-      contact.innerHTML = `<b>${office.contactName}</b>`;
-      contact.innerHTML += `<br/>${office.contactTitle}`;
-      contact.innerHTML += `<br/>${office.contactPhone}`;
-      contact.innerHTML += `<br/>${office.contactEmail}`;
-      const contactBtn = document.createElement('p');
-      contactBtn.className = 'button-container';
-      contactBtn.innerHTML = `<a href="/offices/${toClassName(office.location)}" title="Visit Us" class="button">Visit Us</a>`;
-      cardBody.append(location, address, contact, contactBtn);
-      cardsItem.append(cardBody);
-      list.append(cardsItem);
-    });
+    if (url.searchParams.has('sheet')) {
+      buildAgentCards(list, jsonData);
+    } else {
+      buildOfficeCards(list, jsonData);
+    }
   } else {
     buildCards(block, cards);
   }
